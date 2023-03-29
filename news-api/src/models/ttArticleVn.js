d57af0d32635fc603408976a71c59e57186fec3b
Đức Đào <duc.dao.431@gmail.com>
2023-03-29 15:07:06 +0700

    🎈 perf(model): update ttArticlesVn

--- news-api/src/models/ttArticleVn.js
+++ news-api/src/models/ttArticleVn.js
@@ -5,11 +5,11 @@ const ttVnArticleSchema = new Schema({
 		id: {
 			type: String,
 			required: true,
+			unique: true,
 		},
 		url: {
 			type: String,
 			required: true,
-			unique: true,
 		},
 		category: {
 			type: String,
@@ -31,9 +31,39 @@ const ttVnArticleSchema = new Schema({
 				},
 			},
 		],
-		publish_date: {
-			type: Date,
-			required: true,
+		pubdate: {
+			year: {
+				type: String,
+			},
+			month: {
+				type: String,
+			},
+			day: {
+				type: String,
+			},
+			hour: {
+				type: String,
+			},
+			minute: {
+				type: String,
+			},
+			isodate: {
+				type: Date,
+				default: function () {
+					return Date(
+						this.metadata.pubdate.year +
+							"-" +
+							this.metadata.pubdate.month +
+							"-" +
+							this.metadata.pubdate.day +
+							"T" +
+							this.metadata.pubdate.hour +
+							":" +
+							this.metadata.pubdate.minute +
+							":00+07:00"
+					);
+				},
+			},
 		},
 		authors: [
 			{
@@ -50,6 +80,10 @@ const ttVnArticleSchema = new Schema({
 		{
 			tag: {
 				type: String,
+			},
+			type: {
+				type: String,
+				enum: ["text", "texta", "image", "video", "audio", "iframe", "quote", "highlight"], // texta: text with attributes
 			},
 			content: {
 				type: String,

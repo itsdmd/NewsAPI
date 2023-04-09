6068fa9d0ab485e1f883f835fdf202b3ff1521b6
Đức Đào <duc.dao.431@gmail.com>
2023-04-09 19:59:07 +0700

    ✨ feat(refetch): log scrape status to local file

--- news-api/src/scripts/refetch.js
+++ news-api/src/scripts/refetch.js
@@ -1,11 +1,34 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import * as scraper from "./scraper/scraper.js";
+
+import * as fs from "fs";
+const logFileDir = process.env.REFETCH_LOG_FILE_PATH;
 
 async function scrape(mode, baseUrl, startUrl, limit = 1) {
 	/* ------------ scraping ------------ */
 	await scraper
 		.scrape(mode, baseUrl, startUrl, limit)
+		.then(() => {
+			const logData = new Date().toISOString() + " " + mode + " " + limit + " || Success\n";
+
+			fs.appendFile(logFileDir, logData, (error) => {
+				if (error) {
+					console.log("\n[refetch:scrape] Error appendFile: " + error.message);
+				}
+			});
+		})
 		.catch((error) => {
 			console.log("\n[refetch:scrape] Error: " + error.message);
+
+			const logData = new Date().toISOString() + " " + mode + " " + limit + " || Error: " + error.message + "\n";
+
+			fs.appendFile(logFileDir, logData, (error) => {
+				if (error) {
+					console.log("\n[refetch:scrape] Error appendFile: " + error.message);
+				}
+			});
 		})
 		.finally(async () => {
 			console.log("\n[refetch:scrape] Done scraping");

5a0330f766fff5099790ed32c205dde33fbdb9fa
Đức Đào <duc.dao.431@gmail.com>
2023-04-13 15:50:06 +0700

    ✨ feat(query): query from all collections

--- news-api/src/scripts/query.js
+++ news-api/src/scripts/query.js
@@ -3,8 +3,9 @@
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
 
-// import ttVnModel from "../models/ttArticleVn.js";
+import ttVnModel from "../models/ttArticleVn.js";
 import tnVnModel from "../models/tnArticleVn.js";
+import vnxVnModel from "../models/vnxArticleVn.js";
 
 dotenv.config();
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
@@ -18,10 +19,28 @@ db.once("open", async () => {
 
 const limit = 10;
 export async function query(input, filter = { _id: 0, content: 0 }, offset = 0) {
-	const result = await tnVnModel
+	const res_tt = await ttVnModel
 		.find(input, filter)
 		.sort("metadata.pubdate.isodate")
-		.skip(offset * limit)
-		.limit(limit);
-	return result;
+		.limit(offset * limit);
+
+	const res_tn = await tnVnModel
+		.find(input, filter)
+		.sort("metadata.pubdate.isodate")
+		.limit(offset * limit);
+
+	const res_vnx = await vnxVnModel
+		.find(input, filter)
+		.sort("metadata.pubdate.isodate")
+		.limit(offset * limit);
+
+	const result = res_tt.concat(res_tn, res_vnx);
+
+	result.sort((a, b) => {
+		const aDate = new Date(a.metadata.pubdate.isodate);
+		const bDate = new Date(b.metadata.pubdate.isodate);
+		return aDate - bDate;
+	});
+
+	return result.slice(offset * limit, (offset + 1) * limit);
 }
